664e05d0344adb546f346cd4ebc64bd6bdf4a006
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-02-18 10:58:59 +0100

    fix relative paths

--- rpc-demos/start.sh
+++ rpc-demos/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Heroku start script
 if [ "$TARGET" = "secure" ]; then
-  sh rpc-demo-secure-jar/target/bin/exec
+  sh rpc-demos/rpc-demo-secure-jar/target/bin/exec
 else
- sh rpc-demo-jar/target/bin/exec
+ sh rpc-demos/rpc-demo-jar/target/bin/exec
 fi;
