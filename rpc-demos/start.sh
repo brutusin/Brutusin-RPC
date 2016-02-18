888515de5ec9feb77e44d553ee571c544b8359c0
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-02-18 11:06:22 +0100

    update

--- rpc-demos/start.sh
+++ rpc-demos/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Heroku start script
 if [ "$TARGET" = "secure" ]; then
-  sh rpc-demos/rpc-demo-secure-jar/target/bin/exec
+  sh rpc-demos/rpc-demo-security-jar/target/bin/exec
 else
  sh rpc-demos/rpc-demo-jar/target/bin/exec
 fi;
