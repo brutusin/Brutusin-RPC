8cbc79aad7b48b8e22ffeff3321e197f2641b530
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-03-28 16:43:00 +0200

    Update start.sh

--- rpc-demos/start.sh
+++ rpc-demos/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Heroku start script
-export org.brutusin.rpc.link-source-code=true
+export BRUTUSIN_RPC_LINK_SOURCE=true
 if [ "$TARGET" = "secure" ]; then
   sh rpc-demos/rpc-demo-security-jar/target/bin/exec
 elif [ "$TARGET" = "jar" ]; then
