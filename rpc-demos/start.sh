da3efeb1b380ec12cbf84bc1dcf313b899e3e77b
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-04-04 15:06:55 +0200

    create executable with static name

--- rpc-demos/start.sh
+++ rpc-demos/start.sh
@@ -2,10 +2,10 @@
 # Heroku exec script
 export BRUTUSIN_RPC_LINK_SOURCE=true
 if [ "$TARGET" = "secure" ]; then
-  cd rpc-demos/rpc-demo-security-jar
+  cd rpc-demos/rpc-demo-security-jar/target
 elif [ "$TARGET" = "jar" ]; then
-  cd rpc-demos/rpc-demo-jar
+  cd rpc-demos/rpc-demo-jar/target
 elif [ "$TARGET" = "chat" ]; then
-  cd rpc-demos/rpc-chat
+  cd rpc-demos/rpc-chat/target
 fi;
-mvn exec:java -Dexec.mainClass=org.brutusin.rpc.Main
+java -jar executable.jar
