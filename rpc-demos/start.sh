71866e626cc2506508928c1e9c690d91ebc9a0a4
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-04-04 14:37:00 +0200

    exec with maven

--- rpc-demos/start.sh
+++ rpc-demos/start.sh
@@ -3,11 +3,9 @@
 export BRUTUSIN_RPC_LINK_SOURCE=true
 if [ "$TARGET" = "secure" ]; then
   cd rpc-demos/rpc-demo-security-jar
-  mvn exec:java -Dexec.mainClass=org.brutusin.rpc.Main
 elif [ "$TARGET" = "jar" ]; then
   cd rpc-demos/rpc-demo-jar
-  mvn exec:java -Dexec.mainClass=org.brutusin.rpc.Main
 elif [ "$TARGET" = "chat" ]; then
   cd rpc-demos/rpc-chat
-  mvn exec:java -Dexec.mainClass=org.brutusin.rpc.Main
 fi;
+mvn exec:java -Dexec.mainClass=org.brutusin.rpc.Main
