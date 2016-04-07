212ccc80143efe74e52b598cecc70d4bbf3c23a7
Ignacio del Valle Alles <idelvall@brutusin.org>
2016-04-07 09:13:00 +0200

    use blob as responseType

--- rpc-impl/src/main/resources/META-INF/resources/rpc/brutusin-rpc.js
+++ rpc-impl/src/main/resources/META-INF/resources/rpc/brutusin-rpc.js
@@ -200,55 +200,19 @@ if (typeof brutusin === "undefined") {
                 }
             }
 
-            function Utf8ArrayToStr(array) {
-                var out, i, len, c;
-                var char2, char3;
-                out = "";
-                len = array.length;
-                i = 0;
-                while (i < len) {
-                    c = array[i++];
-                    switch (c >> 4)
-                    {
-                        case 0:
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 7:
-                            // 0xxxxxxx
-                            out += String.fromCharCode(c);
-                            break;
-                        case 12:
-                        case 13:
-                            // 110x xxxx   10xx xxxx
-                            char2 = array[i++];
-                            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-                            break;
-                        case 14:
-                            // 1110 xxxx  10xx xxxx  10xx xxxx
-                            char2 = array[i++];
-                            char3 = array[i++];
-                            out += String.fromCharCode(((c & 0x0F) << 12) |
-                                    ((char2 & 0x3F) << 6) |
-                                    ((char3 & 0x3F) << 0));
-                            break;
-                    }
-                }
-                return out;
-            }
-
             xhr.onload = function () {
                 var type = xhr.getResponseHeader('Content-Type');
                 if (type.startsWith("application/json")) {
                     if (load) {
-                        var response;
-                        eval("response=" + Utf8ArrayToStr(new Uint8Array(this.response)));
-                        load(response, this.status, this.statusText);
+                        var fileReader = new FileReader();
+                        fileReader.onload = function () {
+                            var response;
+                            eval("response=" + this.result);
+                            load(response, xhr.status, xhr.statusText);
+                        };
+                        fileReader.readAsText(this.response);
                     }
-                } else { 
+                } else {
                     var blob = new Blob([this.response], {type: type});
                     var doDefault = true;
                     if (load) {
@@ -298,7 +262,7 @@ if (typeof brutusin === "undefined") {
                     }
                 });
             };
-            xhr.responseType = "arraybuffer";
+            xhr.responseType = "blob";
             xhr.send(data);
         }
     };
